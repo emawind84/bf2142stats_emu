2cc42ee96ec874efc13039c2196cba105b9fec21
Emanuele Disco <emawind84@gmail.com>
2025-09-08 16:37:36 +0900

    restore db version check

--- web/index.php
+++ web/index.php
@@ -8,10 +8,12 @@ ob_start();
 | Define Constants
 | ---------------------------------------------------------------
 */
+define('_BF2142_ADMIN', '1');
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__) . DS);
 define('SYSTEM_PATH', ROOT . DS . 'system');
 define('SNAPSHOT_TEMP_PATH', SYSTEM_PATH . DS . 'snapshots' . DS . 'temp');
+DEFINE("_ERR_RESPONSE", "E\nH\tresponse\nD\t<font color=\"red\">ERROR</font>: ");
 
 /*
 | ---------------------------------------------------------------
@@ -57,14 +59,12 @@ $allow_db_show = true;
 set_time_limit(0);
 ignore_user_abort(true);
 
-define('_BF2142_ADMIN', '1');
 require(ROOT . DS . 'include' . DS . '_ccconfig.php');
 require(ROOT . DS . 'include' . DS . 'utils.php');
 require(SYSTEM_PATH . DS . 'core'. DS .'AutoLoader.php');
 require(SYSTEM_PATH . DS . 'functions.php');
 
 $cfg = new Config();
-DEFINE("_ERR_RESPONSE", "E\nH\tresponse\nD\t<font color=\"red\">ERROR</font>: ");
 
 // Open database connection
 $connection = @mysql_connect($db_host, $db_user, $db_pass);
@@ -73,17 +73,16 @@ $connection = @mysql_connect($db_host, $db_user, $db_pass);
 
 // Check Database Version
 $dbver = getDbVer();
-error_log(">>> DB VERSION: {$dbver}");
-//$curdbver = getDbVer();
-//
-//if ($curdbver != $cfg->get('db_expected_ver')) {
-//	$errmsg = "Database version expected: ".$cfg->get('db_expected_ver').", Found: {$curdbver}";
-//	ErrorLog($errmsg, 1);
-//	die();
-//} else {
-//	$errmsg = "Database version expected: ".$cfg->get('db_expected_ver').", Found: {$curdbver}";
-//	ErrorLog($errmsg, 3);
-//}
+error_log(">>> DB VERSION: {$dbver}, expected: {$cfg->get('db_expected_ver')}");
+
+if ($dbver != $cfg->get('db_expected_ver')) {
+	$errmsg = "Database version expected: ".$cfg->get('db_expected_ver').", Found: {$dbver}";
+	ErrorLog($errmsg, 1);
+	die("<font color='red'>ERROR:</font> {$errmsg}");
+} else {
+	$errmsg = "Database version expected: ".$cfg->get('db_expected_ver').", Found: {$dbver}";
+	ErrorLog($errmsg, 3);
+}
 @mysql_close($connection);
 
 // Register AutoLoader
